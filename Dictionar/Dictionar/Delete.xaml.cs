8454de5ceb0175e3951f932821f164ac3c1d83bc
DumaVlad <56892207+DumaVlad@users.noreply.github.com>
2021-05-15 15:29:25 +0300

    Add files via upload

---
+++ Dictionar/Dictionar/Delete.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dictionar
+{
+    /// <summary>
+    /// Interaction logic for Delete.xaml
+    /// </summary>
+    public partial class Delete : Window
+    {
+        public Delete()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Administrative_Module am = new Administrative_Module();
+            this.Close();
+            am.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string w = cb.Text;
+
+            Word aux_word = (this.DataContext as Autocomplete).SearchWord(w);
+
+            string rm_line = aux_word.text + "," + aux_word.category + "," + aux_word.description + "," + aux_word.picture_path;
+            Console.WriteLine(rm_line);
+
+            var tempFile = @"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile2.txt";
+            var linesToKeep = File.ReadLines(@"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile1.txt").Where(l => l != rm_line);
+
+            File.WriteAllLines(tempFile, linesToKeep);
+
+            File.Delete(@"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile1.txt");
+            File.Move(@"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile2.txt", @"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile1.txt");
+
+            FileStream fs = new FileStream(@"C:\\Users\\vladd\\OneDrive\\Documents\\Facultate\\AN 2\\MVP\\Team 1\\Dictionar\\TextFile1.txt", FileMode.Open, FileAccess.ReadWrite);
+            fs.SetLength(fs.Length - 1);
+            fs.Close();
+
+        }
+    }
+}
---
+++ Dictionar/Dictionar/Entertainment_Module.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Dictionar
+{
+    /// <summary>
+    /// Interaction logic for Entertainment_Module.xaml
+    /// </summary>
+    public partial class Entertainment_Module : Window
+    {
+        public List<string> answears;
+        public List<string> ranswears;
+        public int numberOfQuestions = new int();
+        private ObservableCollection<Word> ceva;
+        public Entertainment_Module()
+        {
+            InitializeComponent();
+            res.Visibility = System.Windows.Visibility.Hidden;
+            ceva = new ObservableCollection<Word>();
+
+            answears = new List<string>();
+            ranswears = new List<string>();
+
+            numberOfQuestions = 0;
+            Random random = new System.Random();
+            Autocomplete x = new Autocomplete();
+            ceva = x.word_list;
+
+            int value1 = random.Next(0, ceva.Count());
+            Word aux_word = ceva[value1];
+            ranswears.Add(aux_word.text);
+            
+            int value = random.Next(0, 2);
+            
+            if (value == 0)
+            {
+                des.Text = aux_word.description;
+                des.Visibility = System.Windows.Visibility.Visible;
+                lb.Content = "Descriere";
+                img.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                img.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(aux_word.picture_path);
+                lb.Content = "    Imagine";
+                img.Visibility = System.Windows.Visibility.Visible;
+                des.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            this.Close();
+            menu.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string a = answ.Text;
+            answears.Add(a);
+            numberOfQuestions++;
+            answ.Text = "";
+
+            Random random = new System.Random();
+            int value1 = random.Next(0, ceva.Count());
+
+            Word aux_word = ceva[value1];
+            ranswears.Add(aux_word.text);
+            int value = random.Next(0, 2);
+
+            if (value == 0)
+            {
+                des.Text = aux_word.description;
+                des.Visibility = System.Windows.Visibility.Visible;
+                lb.Content = "Description";
+                img.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else
+            {
+                img.Source = (ImageSource)new ImageSourceConverter().ConvertFromString(aux_word.picture_path);
+                lb.Content = "     Image";
+                img.Visibility = System.Windows.Visibility.Visible;
+                des.Visibility = System.Windows.Visibility.Hidden;
+            }
+
+            if (numberOfQuestions == 4)
+            {
+                next.Content = "Final joc";
+            }
+
+            if (numberOfQuestions == 5)
+            {
+                next.Visibility = System.Windows.Visibility.Hidden;
+                des.Visibility = System.Windows.Visibility.Hidden;
+                img.Visibility = System.Windows.Visibility.Hidden;
+                answ.Visibility = System.Windows.Visibility.Hidden;
+                lb.Visibility = System.Windows.Visibility.Hidden;
+                res.Visibility = System.Windows.Visibility.Visible;
+                string results = "";
+                for(int index = 0; index< 5; index++)
+                {
+                    if (answears[index] == ranswears[index])
+                        results += "Intrebarea " + (index+1) + ": Raspuns corect!" + "\n";
+                    else
+                        results += "Intrebarea " + (index+1) + ": Raspuns gresit!" + " Raspunsul corect este: " + ranswears[index] + "\n";
+                }
+                Console.WriteLine(results);
+                res.Text = results;
+            }
+            
+        }
+    }
+}
